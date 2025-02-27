1a9e8e2688d763d72ddbecbd24725280dc2ce356
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-27 16:37:18 +0100

    add email generation script and refresh button

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@
 
 const emailList = document.querySelector('#emailList')
 const emailButton = document.querySelector('#emailButton')
+let randomEmailValue = null
 
+for (let i = 0; i < 10; i++) {
+    
+}
 fetch('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(response => response.json())
     .then(data => {
-        console.log(data.response);
+        randomEmailValue = data.response
     })
