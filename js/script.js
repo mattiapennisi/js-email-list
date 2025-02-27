b28eb79093663230434bd7365a6aaf3314e5cd12
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-27 16:10:41 +0100

    add basic structure and email api

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // Variables
 
+const emailList = document.querySelector('#emailList')
+const emailButton = document.querySelector('#emailButton')
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.response);
+    })
